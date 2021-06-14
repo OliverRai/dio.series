ac31eb83816a065bfb241609688be9b4106fd71f
oliverrai <raiane.mateus.oliveira@gmail.com>
2021-06-13 21:35:23 -0300

    initial commit project dio series

---
+++ Classes/Series.cs
@@ -0,0 +1,42 @@
+namespace Series
+{
+    public class Series : EntidadeBase
+    {
+        private Genero Genero {get; set;}
+
+        private string Titulo {get; set;}
+
+        private string Descricao {get; set;}
+
+        private int Ano {get; set;}
+    
+        public Series(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero;
+            retorno += "Título: " + this.Titulo;
+            retorno += "Descrição: " + this.Descricao;
+            retorno += "Ano: " + this.Ano;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+    }
+}
---
+++ Enum/Genero.cs
@@ -0,0 +1,25 @@
+namespace Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+
+        Aventura = 2,
+
+        Comedia = 3,
+
+        Documentario = 4,
+
+        Drana = 5,
+
+        Espionagem = 6,
+
+        Faroesta = 7,
+
+        Fantasia = 8,
+
+        Ficcao_Cientifica = 9,
+
+        Romance = 10,
+    }
+}
